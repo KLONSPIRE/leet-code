3bda8ad75993529a938661a7eb839db510933f58
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-01 16:57:08 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/509. Fibonacci Number/Solution.cpp
+++ LeetCode/C++/Easy/509. Fibonacci Number/Solution.cpp
@@ -4,8 +4,6 @@ public:
         int f1 = 0, f2 = 1;
         if(n <= 1)
             return 0;
-        else if(n == 2)
-            return 1;
         else{
             for(int i = 2; i <= n; i++){
                 f2 = f1 + f2;
