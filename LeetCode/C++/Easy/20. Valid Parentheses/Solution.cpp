a2fc9caa2f07ab7f33499276d9cf8124b62bccab
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-10 14:13:16 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/20. Valid Parentheses/Solution.cpp
+++ LeetCode/C++/Easy/20. Valid Parentheses/Solution.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> str;
-        
-        if(s.size() == 1)
-            return 0;
+    
 
         for(int i = 0; i <s.size(); i++){
             if(s[i] == '(' || s[i] == '[' || s[i] == '{')
