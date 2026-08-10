3de5cddfb9cb744592264cf46a441cb2967f3202
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-10 14:12:09 +0530

    Add C++ Solution to Patterns/Stack & Queue

---
+++ LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> str;
+        
+        for(int i = 0; i <s.size(); i++){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{')
+                str.push(s[i]);
+            else{
+                if(str.empty())
+                    return 0;
+                else if ((s[i] == ')' && str.top() == '(') ||
+                    (s[i] == ']' && str.top() == '[') ||
+                    (s[i] == '}' && str.top() == '{'))
+                    str.pop();
+                else
+                    return 0; 
+            }
+        }
+        return 1;
+    }
+};
