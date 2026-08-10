a83bc3929b3be69209d2b1c362a328a4d1981eda
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-10 13:49:37 +0530

    Add C++ Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/344. Reverse String/Solution.cpp
+++ LeetCode/Patterns/Two Pointers/344. Reverse String/Solution.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        stack<char> str;
 
-        for(int i = 0; i < s.size(); i++)
-            str.push(s[i]);
-        for(int i = 0; i < s.size(); i++){
-            s[i] = str.top();
-            str.pop();    
+        for(int i = 0; i < s.size()/2; i++){
+            s[i] = s[i] + s[s.size() - i - 1];
+            s[s.size() - i - 1] = s[i] - s[s.size() - i - 1];
+            s[i] = s[i] - s[s.size() - i - 1];
         }
-        
     }
 };
