16272d68c8a8e7f8e25302aa0b879e9f82145116
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-11 22:17:43 +0530

    Add C++ Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/26. Remove Duplicates from Sorted Array/Solution.cpp
+++ LeetCode/Patterns/Two Pointers/26. Remove Duplicates from Sorted Array/Solution.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        int j = 1;
         for(int i = 1; i < nums.size(); i++)
-            if(nums[i] == nums[i-1]){
-                nums.erase(nums.begin() + i);
-                i--;
+            if(nums[i] != nums[i-1]){
+                nums[j] = nums[i];
+                j++;
             }
         
-        return nums.size();
+        return j;
     }
 };
