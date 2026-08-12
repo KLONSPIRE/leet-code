5276b9f50d54832718e61e71017d515b932a21b5
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-12 22:05:26 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/28. Find the Index of the First Occurrence in a String/Solution.cpp
+++ LeetCode/C++/Easy/28. Find the Index of the First Occurrence in a String/Solution.cpp
@@ -6,7 +6,7 @@ public:
         int i, j;
         for(i = 0; i <= haystack.length() - needle.length(); i++){
             if(haystack[i] == needle[0] && haystack[i+needle.size() - 1] == needle[needle.size() - 1]){
-                for(j = 1; j < needle.size()/2; j++)
+                for(j = 1; j <= needle.size()/2; j++)
                     if(haystack[i+j] != needle[i+j] || haystack[i+needle.size() - 1 - j] != needle[needle.size() - j- 1])
                         break;
                 if(j >= needle.size()/2)
