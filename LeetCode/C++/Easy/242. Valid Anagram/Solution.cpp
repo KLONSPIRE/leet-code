e3901a93878aa609b11016ac6dbd993cacc6c3fb
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-12 22:22:54 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/242. Valid Anagram/Solution.cpp
+++ LeetCode/C++/Easy/242. Valid Anagram/Solution.cpp
@@ -4,12 +4,17 @@ public:
         if(s.size() != t.size())
             return 0;
         unordered_map<char, int> c;
-        for(int i = 0; i < 26; i++){
-            c[char('' + i)] = 0;
-        }
+        for(int i = 0; i < s.size(); i++)
+            c[s[i]]++;
+
+        for(int i = 0; i < t.size(); i++)   
+            if(c[t[i]] != 0)
+                c[t[i]]--;
+            else
+                c[t[i]]++;
 
         for(int i = 0; i < s.size(); i++)
-            if(c[s[i]] % 2 != 0)
+            if(c[s[i]] != 0)
                 return 0;
 
         return 1;
