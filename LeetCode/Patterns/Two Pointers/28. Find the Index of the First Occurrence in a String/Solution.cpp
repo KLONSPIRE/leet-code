2677b438ec29c0a9af23bacb30aabe601d7ce3f4
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-12 21:59:20 +0530

    Add C++ Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/28. Find the Index of the First Occurrence in a String/Solution.cpp
+++ LeetCode/Patterns/Two Pointers/28. Find the Index of the First Occurrence in a String/Solution.cpp
@@ -3,9 +3,9 @@ public:
     int strStr(string haystack, string needle) {
         int i, j;
         for(i = 0; i < haystack.length() - needle.length(); i++){
-            if(haystack[i] == needle[i] && haystack[i+needle.size() - 1] == needle[i+needle.size() - 1]){
+            if(haystack[i] == needle[0] && haystack[i+needle.size() - 1] == needle[needle.size() - 1]){
                 for(j = 1; j < needle.size()/2; j++)
-                    if(haystack[i+j] != needle[i+j] || haystack[i+j+needle.size() - 1] != needle[i+ j + needle.size() - 1])
+                    if(haystack[i+j] != needle[i+j] || haystack[i+needle.size() - 1 - j] != needle[needle.size() - j- 1])
                         break;
                 if(j == needle.size()/2)
                     return i;
