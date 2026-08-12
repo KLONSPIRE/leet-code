0f3086e5dc2c37f5eef59a9d9d1e9ccb59f118ce
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-12 22:16:50 +0530

    Add Easy C++ Solution from LeetCode

---
+++ LeetCode/C++/Easy/242. Valid Anagram/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+            return 0;
+        unordered_map<char, int> c;
+        for(int i = 0; i < s.size(); i++){
+            c[s[i]]++;
+            c[t[i]]++;
+        }
+
+        for(int i = 0; i < s.size(); i++)
+            if(c[s[i]] % 2 != 0)
+                return 0;
+
+        return 1;
+    }
+};
