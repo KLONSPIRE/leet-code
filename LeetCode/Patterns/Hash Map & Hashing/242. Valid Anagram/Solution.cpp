0e66320ef759a9dba2d9e14993fe7ac5ab124708
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-12 22:25:27 +0530

    Add C++ Solution to Patterns/Hash Map & Hashing

--- LeetCode/Patterns/Hash Map & Hashing/242. Valid Anagram/Solution.cpp
+++ LeetCode/Patterns/Hash Map & Hashing/242. Valid Anagram/Solution.cpp
@@ -8,7 +8,13 @@ public:
             c[s[i]]++;
 
         for(int i = 0; i < t.size(); i++)   
-            if(c[t[i]] == 0)
+            if(c[t[i]] != 0)
+                c[t[i]]--;
+            else
+                c[t[i]]++;
+
+        for(int i = 0; i < s.size(); i++)
+            if(c[s[i]] != 0)
                 return 0;
 
         return 1;
