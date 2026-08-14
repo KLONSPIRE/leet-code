eec181120b9650c97ad269d81fa76b8818573f8c
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-15 00:23:24 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/1502. Can Make Arithmetic Progression From Sequence/Solution.cpp
+++ LeetCode/C++/Easy/1502. Can Make Arithmetic Progression From Sequence/Solution.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
-        
+
         long long sum = arr[0];
         int low = arr[0], high = arr[0];
+
         for(int i = 1; i < arr.size(); i++){
-            if(arr[i] < low)
-                low = arr[i];
-            if(arr[i] > high)
-                high = arr[i];
+            low = min(low, arr[i]);
+            high = max(high, arr[i]);
             sum += arr[i];
         }
 
-        if((high - low)%(arr.size()-1) != 0)
+        int n = (int)arr.size();
+
+        if((high - low) % (n - 1) != 0)
             return false;
 
-        int d = (high - low)/(arr.size()-1);
-        if(d==0)
+        int d = (high - low) / (n - 1);
+
+        if(d == 0)
             return true;
-        
-        if(sum != 1LL * (long long)(arr.size()) * (low + high) / 2)
+
+        if(sum != 1LL * n * (low + high) / 2)
             return false;
-            
-        for(int i = 0; i < arr.size(); i++)
-            if((arr[i] - low) % d != 0)
+
+        unordered_set<int> seen;
+
+        for(int x : arr) {
+            if((x - low) % d != 0)
                 return false;
+
+            if(!seen.insert(x).second)
+                return false;
+        }
 
         return true;
     }
