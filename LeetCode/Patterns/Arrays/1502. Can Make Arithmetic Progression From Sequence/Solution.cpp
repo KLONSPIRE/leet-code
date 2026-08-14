201aae8f24f7e9c49b39da70624c90b0926a5084
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-14 23:39:32 +0530

    Add C++ Solution to Patterns/Arrays

---
+++ LeetCode/Patterns/Arrays/1502. Can Make Arithmetic Progression From Sequence/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        if(arr.size() == 2)
+            return true;
+        int low = arr[0], high = arr[0];
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i] < low)
+                low = arr[i];
+            if(arr[i] > high)
+                high = arr[i];
+        }
+
+        if((high - low)%(arr.size()-1) != 0)
+            return false;
+            
+        int d = (high - low)/(arr.size()-1);
+        for(int i = 1; i < arr.size(); i++)
+            if((arr[i] - arr[0]) % d != 0)
+                return false;
+
+        return true;
+    }
+};
