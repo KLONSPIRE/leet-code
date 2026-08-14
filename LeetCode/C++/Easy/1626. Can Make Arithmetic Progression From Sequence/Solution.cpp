88e176923ec5e149ab4ca3966e0f77e92d1e7fda
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-14 23:55:48 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/1626. Can Make Arithmetic Progression From Sequence/Solution.cpp
+++ LeetCode/C++/Easy/1626. Can Make Arithmetic Progression From Sequence/Solution.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
-        if(arr.size() == 2)
-            return true;
+        
         int low = arr[0], high = arr[0];
         for(int i = 1; i < arr.size(); i++){
             if(arr[i] < low)
@@ -17,8 +16,9 @@ public:
         int d = (high - low)/(arr.size()-1);
         if(d==0)
             return true;
+        
         for(int i = 1; i < arr.size(); i++)
-            if((arr[i] - arr[0]) % d != 0)
+            if((arr[i] - low) % d != 0)
                 return false;
 
         return true;
