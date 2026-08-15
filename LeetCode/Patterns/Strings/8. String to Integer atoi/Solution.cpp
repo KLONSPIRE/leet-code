178ce3385e08103e451b8213196137351a4a310a
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-15 20:01:21 +0530

    Add C++ Solution to Patterns/Strings

---
+++ LeetCode/Patterns/Strings/8. String to Integer atoi/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int ans = 0, sign = 1;
+        bool nozero = true;
+        for(int i = 0; i < s.size(); i++)
+            if((s[i] > '0' && s[i] <= '9') || (s[i] == '0' && ans != 0)){
+                ans *= 10;
+                ans += s[i] - '0';
+            }
+            else if(s[i] == '0' && ans == 0)
+                nozero = false;
+            else if(s[i] == '-' && nozero == true)
+                sign *= -1;
+            else if(s[i] == ' ' && ans == 0)
+                continue;
+            else
+                break;
+
+        return ans*sign;
+    }
+};
