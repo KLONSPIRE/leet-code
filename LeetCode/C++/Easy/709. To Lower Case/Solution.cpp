13ef4e75cd3d9a5c0a94024c35bd73de39add65b
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-15 12:27:33 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/709. To Lower Case/Solution.cpp
+++ LeetCode/C++/Easy/709. To Lower Case/Solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string toLowerCase(string s) {
         for(int i = 0; i < s.size(); i++)
-            if(s[i] < 'a')
+            if(s[i] > 'a')
                 s[i] += 32;
 
        return s; 
