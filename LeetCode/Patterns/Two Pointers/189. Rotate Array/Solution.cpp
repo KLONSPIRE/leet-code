da867d17fdd0165eabbef8a1bf119430e4871a87
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-16 18:37:38 +0530

    Add C++ Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/189. Rotate Array/Solution.cpp
+++ LeetCode/Patterns/Two Pointers/189. Rotate Array/Solution.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        if(nums.size() < k)
-            return;
-        vector<int> ans;
-        for(int i = nums.size() - k; i < nums.size(); i++)
-            ans.push_back(nums[i]);
-        for(int i = 0; i < nums.size() - k; i++){
-            ans.push_back(nums[i]);
-            nums[i] = ans[i];
-        }
-        for(int i = nums.size() - k; i < nums.size(); i++)
-            nums[i] = ans[i];
+        for
     }
 };
