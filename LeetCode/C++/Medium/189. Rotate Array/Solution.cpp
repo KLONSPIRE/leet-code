184d114f894f9187b4f9594574dfc074cbe55490
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-16 18:40:09 +0530

    Add Medium C++ Solution from LeetCode

--- LeetCode/C++/Medium/189. Rotate Array/Solution.cpp
+++ LeetCode/C++/Medium/189. Rotate Array/Solution.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        for
+        for(int i = 0; i < k; i++)
+            for(int j = 0; j < nums.size() - 1; j++)
+                swap(nums[nums.size() - 1], nums[j]);
     }
 };
