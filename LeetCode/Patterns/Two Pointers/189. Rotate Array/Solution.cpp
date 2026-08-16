7f5c34f640864424945465571b18769814e420b5
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-16 19:07:53 +0530

    Add C++ Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/189. Rotate Array/Solution.cpp
+++ LeetCode/Patterns/Two Pointers/189. Rotate Array/Solution.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         k = k % nums.size();
-        for(int i = 0; i < nums.size() / 2; i++)
-            swap(nums[i], nums[nums.size() - i - 1]);
+        for(int i = 0; i < nums.size() / 2; i++){
+            nums[i] = nums[i] + nums[nums.size() - i - 1];
+            nums[nums.size() - i - 1] = nums[i] - nums[nums.size() - i - 1];
+            nums[i] = nums[i] - nums[nums.size() - i - 1];
+        }
         
-        for(int i = 0; i < k / 2; i++)
-            swap(nums[i], nums[k - i - 1]);
+        for(int i = 0; i < k / 2; i++){
+            nums[i] = nums[i] + nums[k - i - 1];
+            nums[k - i - 1] = nums[i] - nums[k - i - 1];
+            nums[i] = nums[i] - nums[k - i - 1];
+        }
 
         int beg = k, end = nums.size() - 1;
         while(beg < end){
-            swap(nums[beg], nums[end]);
+            nums[beg] = nums[beg] + nums[end];
+            nums[end] = nums[beg] - nums[end];
+            nums[beg] = nums[beg] - nums[end];
             beg++;
             end--;
         }
