b8c9ba3dcdeda26db1673c2ef60fb0cd30e06219
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-16 19:23:22 +0530

    Add C++ Solution to Patterns/Bit Manipulation

--- LeetCode/Patterns/Bit Manipulation/136. Single Number/Solution.cpp
+++ LeetCode/Patterns/Bit Manipulation/136. Single Number/Solution.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if(nums.size() == 1)
-            return nums[0];
-        sort(nums.begin(), nums.end());
-        int i;
-        for( i = 1; i < nums.size() - 1; i = i + 2)
-            if(nums[i] != nums[i-1])
-                break;
+        int ans = 0;
+        for(int i : nums)
+            ans ^= i;
 
-        if(nums[i] != nums[i+1])
-            return nums[i];
-        return nums[i-1];
+        return ans; 
     }
 };
