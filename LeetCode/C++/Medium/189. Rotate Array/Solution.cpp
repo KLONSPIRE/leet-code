48461554c7eacc02ec812e4723ee9677a6c2fe42
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-16 18:57:46 +0530

    Add Medium C++ Solution from LeetCode

--- LeetCode/C++/Medium/189. Rotate Array/Solution.cpp
+++ LeetCode/C++/Medium/189. Rotate Array/Solution.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int> ans;
-        for(int i = nums.size() - k%nums.size(); i < nums.size(); i++)
-            ans.push_back(nums[i]);
-        for(int i = 0; i < nums.size() - k%nums.size(); i++){
-            ans.push_back(nums[i]);
-            nums[i] = ans[i];
+        for(int i = 0; i < nums.size() / 2; i++)
+            swap(nums[i], nums[nums.size() - i - 1]);
+
+        cout << endl;
+        for(int i = 0; i < nums.size(); i++)
+            cout << nums[i] << " ";
+        
+        for(int i = 0; i < (k % nums.size()) / 2; i++)
+            swap(nums[i], nums[k % nums.size() - i - 1]);
+
+        cout << endl;
+        for(int i = 0; i < nums.size(); i++)
+            cout << nums[i] << " ";
+
+        int beg = k % nums.size(), end = nums.size() - 1;
+        while(beg < end){
+            swap(nums[beg], nums[end]);
+            beg++;
+            end--;
         }
-        for(int i = nums.size() - k%nums.size(); i < nums.size(); i++)
-            nums[i] = ans[i];
     }
 };
