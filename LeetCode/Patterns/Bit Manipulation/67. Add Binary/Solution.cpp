d604a2dff318a7964ed408e410ffc05baaf708bf
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-19 21:44:51 +0530

    Add C++ Solution to Patterns/Bit Manipulation

--- LeetCode/Patterns/Bit Manipulation/67. Add Binary/Solution.cpp
+++ LeetCode/Patterns/Bit Manipulation/67. Add Binary/Solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int x = 0, y = 0;
+        long lo x = 0, y = 0;
         int pos = 0;
         for(int i = a.size() - 1; i >= 0; i--){
             x += int(a[i] - '0')*pow(2, pos);
