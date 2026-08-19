c24a42d49759358fe7c2af060799b1f1515da42f
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-19 21:44:13 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/67. Add Binary/Solution.cpp
+++ LeetCode/C++/Easy/67. Add Binary/Solution.cpp
@@ -7,18 +7,23 @@ public:
             x += int(a[i] - '0')*pow(2, pos);
             pos++;
         }
+        cout << x << endl;
         pos = 0;
         for(int i = b.size() - 1; i >= 0; i--){
             y += int(b[i] - '0')*pow(2, pos);
             pos++;
         }
+        cout << y << endl;
         x += y;
         string ans = "";
         while(x > 1){
             ans.push_back(char(x % 2 + '0'));
             x /= 2;
         }
-        ans.push_back('1');
+        if(x != 0)
+            ans.push_back('1');
+        else
+            ans.push_back('0');
         for(int i = 0; i < ans.size()/2; i++){
             char temp = ans[i];
             ans[i] = ans[ans.size() - 1 - i];
