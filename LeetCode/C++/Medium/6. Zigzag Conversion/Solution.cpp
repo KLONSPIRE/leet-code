d18129bc089c8048f37a0af0db9f49474bbb40cb
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-20 23:39:40 +0530

    Add Medium C++ Solution from LeetCode

---
+++ LeetCode/C++/Medium/6. Zigzag Conversion/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        bool lower = true;
+        string str = "";
+
+        for (int i = 1; i <= numRows; i++){
+            int j = 0;
+            for (int k = 0; k < s.size(); k++) {
+                if (lower == true)
+                    j++;
+                else
+                    j--;
+                if (j == i)
+                    str.push_back(s[k]);
+                if (j == numRows)
+                    lower = false;
+                else if (j == 1)
+                    lower = true;
+            }
+        }
+        return str;
+    }
+};
