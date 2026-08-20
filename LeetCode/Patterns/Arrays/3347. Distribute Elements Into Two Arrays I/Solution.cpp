67bd36dfa792518101a88fbce6678f2bfe2520e8
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-20 22:46:37 +0530

    Add C++ Solution to Patterns/Arrays

--- LeetCode/Patterns/Arrays/3347. Distribute Elements Into Two Arrays I/Solution.cpp
+++ LeetCode/Patterns/Arrays/3347. Distribute Elements Into Two Arrays I/Solution.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     vector<int> resultArray(vector<int>& nums) {
         vector<int> a, b;
-        int i;
-        for(i = 0; i < nums.size()-1; i++){
-            if(i % 2 == 0)
-                a.push_back(nums[i]);
-            else
-                b.push_back(nums[i]);
-        }
-        if(a.back() > b.back())
-            a.push_back(nums[i]);
-        else
-            b.push_back(nums[i]);
+        int i = 0;
+        a.push_back(nums[i++]);
+        b.push_back(nums[i++]);
         
-        for(int i = 0; i < b.size(); i++)
+        while(i < nums.size())
+            if(a.back() > b.back())
+            a.push_back(nums[i++]);
+        else
+            b.push_back(nums[i++]);
+        
+        for(i = 0; i < b.size(); i++)
             a.push_back(b[i]);
         
         return a;
