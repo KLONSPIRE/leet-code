ceae1113f7bf2f1d85d89adc356da63fa85fcb11
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-21 15:27:04 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/1013. Fibonacci Number/Solution.cpp
+++ LeetCode/C++/Easy/1013. Fibonacci Number/Solution.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     int fib(int n) {
         int f1 = 0, f2 = 1;
-        if(n <= 0)
+        if(n <= 1)
             return 0;
-        else if(n == 1)
-            return 1;
         else{
             for(int i = 2; i <= n; i++){
                 f2 = f1 + f2;
