6743a7229fc15c6384ebfc549642de0b2aa2f64f
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-21 15:16:33 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/326. Power of Three/Solution.cpp
+++ LeetCode/C++/Easy/326. Power of Three/Solution.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
+        if(n <= 0)
+            return false;
+        
         if(n == 1)
             return true;
-        else if(n == 0)
-            return false;
         
         return (n % 3 == 0) && (isPowerOfThree(n / 3) == true);
     }
