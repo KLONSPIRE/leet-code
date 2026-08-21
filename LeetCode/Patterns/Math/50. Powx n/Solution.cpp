08f592fbba916613693f69ea69c6588693c64360
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-21 15:06:29 +0530

    Add C++ Solution to Patterns/Math

--- LeetCode/Patterns/Math/50. Powx n/Solution.cpp
+++ LeetCode/Patterns/Math/50. Powx n/Solution.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n == 0)
+        if((long long)n == 0)
             return 1;
         
         if(n < 0)
-            return 1 / myPow(x, -(long long)n);
+            return 1 / myPow(x, n*-1);
         
         double half = myPow(x, n/2);
         if(n % 2 == 0)
