376387d54772f5872ae64d4f15212894f6d2585e
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-24 14:28:06 +0530

    Add C++ Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/70. Climbing Stairs/Solution.cpp
+++ LeetCode/Patterns/Dynamic Programming/70. Climbing Stairs/Solution.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if(n == 1)
-            return 1;
-        int dp[n];
-        dp[0] = 1;
-        dp[1] = 2;
-        for(int i = 2; i < n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
+    int solve(int n, vector<int>& dp){
+        if(n <= 2)
+            return n;
+        if(dp[n] != -1)
+            return dp[n];
 
-        return dp[n-1];
+        dp[n] = solve(n-1, dp) + solve(n-2, dp);
+        return dp[n];
+    }
+    
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+
+        return solve(n, dp);
     }
 };
