3793ded138681b291d1827887a3c697e60f7ad69
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-24 14:41:05 +0530

    Add C++ Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/1236. N-th Tribonacci Number/Solution.cpp
+++ LeetCode/Patterns/Dynamic Programming/1236. N-th Tribonacci Number/Solution.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int solve(int n, vector<int>& dp){
+    int tribonacci(int n) {
         if(n == 0)
             return 0;
         if(n <= 2)
             return 1;
-        if(dp[n] != -1)
-            return dp[n];
         
-        dp[n] = solve(n-1, dp) + solve(n-2, dp) + solve(n-3, dp);
-        return dp[n];
-    }
+        int a = 0, b = 1, c = 1;
+        for(int i = 0; i <= n-3; i++){
+            int temp = a+b+c;
+            a = b;
+            b = c;
+            c = temp;
+        }
 
-    int tribonacci(int n) {
-        vector<int> dp(n+1, -1);
-
-        return solve(n, dp);
+        return c;
     }
 };
