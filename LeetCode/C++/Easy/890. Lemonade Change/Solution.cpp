c6c72b2c6f574bd7e5732e118b9a740030e34e1f
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-31 13:50:11 +0530

    Add Easy C++ Solution for 890. Lemonade Change from LeetCode

--- LeetCode/C++/Easy/890. Lemonade Change/Solution.cpp
+++ LeetCode/C++/Easy/890. Lemonade Change/Solution.cpp
@@ -4,21 +4,21 @@ public:
         unordered_map<int, int> bill;
         for(int x : bills){
             bill[x]++;
-            switch(x){
-                case 10:
-                    bill[5]--;
-                    break;
-                case 20:
-                    bill[5]--;
-                    if(bill[10] > 0)
-                        bill[10]--;
-                    else
-                        bill[5] -= 2;
-                    break;
+            if(x == 10){
+                bill[5]--;
+                if(bill[5] < 0)
+                    return false;
+            }
+            if(x == 20){
+                bill[5]--;
+                if(bill[10] > 0)
+                    bill[10]--;
+                else
+                    bill[5] -= 2;
+                if(bill[5] < 0)
+                    return false;
             }
         }
-        if(bill[5] < 0 || bill[10] < 0)
-            return false;
         return true;
     }    
 };
