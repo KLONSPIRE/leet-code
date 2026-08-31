ef105e215eb6cf74848607592653196a9e13468d
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-08-31 13:45:07 +0530

    Add Easy C++ Solution for 890. Lemonade Change from LeetCode

---
+++ LeetCode/C++/Easy/890. Lemonade Change/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        unordered_map<int, int> bill;
+        for(int x : bills){
+            bill[x]++;
+            switch(x){
+                case 10:
+                    bill[5]--;
+                    break;
+                case 20:
+                    bill[5]--;
+                    bill[10]--;
+                    break;
+            }
+        }
+        if(bill[5] < 0 || bill[10] < 0)
+            return false;
+        return true;
+    }    
+};
